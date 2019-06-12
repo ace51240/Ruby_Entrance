5892cb4a8d55433bf14b98a76c2e488308822089
achis cat <takahiro_y@suou.waseda.jp>
2019-06-12 10:02:14 +0900

    変数定数

---
+++ variable.rb
@@ -0,0 +1,13 @@
+# 変数
+msg = "hello world"
+puts msg
+
+msg = "hello world again"
+puts msg
+
+# 定数 頭文字が大文字である必要があるが，慣習として全部大文字として定義
+VERSION = 1.1
+puts VERSION
+
+VERSION = 1.2 # 警告がでる
+puts VERSION
